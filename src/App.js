65748277ad9b04333cb34da7ac14b0741df4b890
Nadeesha Malshan <malshannandanayaka@gmail.com>
2026-01-30 19:44:13 +0530

    edit

--- src/App.js
+++ src/App.js
@@ -1,28 +1,19 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import NavBar from './Components/NavBar';
-import logo from './logo.svg';
+import NavBar from './components/NavBar';
 import './App.css';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 function App() {
   return (
     <Router>
+      <NavBar />
       <div className="App">
-        <NavBar />
+        
         <header className="App-header">
-          <Routes>
-            <Route path="/" element={
-              <>
-                <img src={logo} className="App-logo" alt="logo" />
-                <p>Welcome to the Doctor Channeling System</p>
-              </>
-            } />
-            <Route path="/doctors" element={<h2>Doctors Page</h2>} />
-            <Route path="/login" element={<h2>Login Page</h2>} />
-            <Route path="*" element={<h2>Page Not Found</h2>} />
-          </Routes>
+          <h1>Meet your doctor with (Project Name)</h1>
         </header>
       </div>
     </Router>
+    
   );
 }
 
