ac5ec8ba2b514f67b0cc1b2c85876c7be76f01da
IT24103522 <pamoddil7@gmail.com>
2026-02-19 15:24:20 +0530

    patient dashboard functions

--- src/Components/eCareNavBar.js
+++ src/Components/eCareNavBar.js
@@ -4,6 +4,17 @@ import './ComponentsCss/eCareNavBar.css';
 const ECareNavBar = () => {
     const navigate = useNavigate();
 
+    // Read logged-in user from localStorage
+    const storedUser = localStorage.getItem('user');
+    const user = storedUser ? JSON.parse(storedUser) : null;
+    const patientName = user ? (user.firstName || user.name || user.fullName || user.email) : null;
+
+    const handleLogout = () => {
+        localStorage.removeItem('user');
+        localStorage.removeItem('userType');
+        navigate('/login');
+    };
+
     return (
         <nav className="ecare-navbar">
             <div className="ecare-navbar-brand">
@@ -25,18 +36,38 @@ const ECareNavBar = () => {
                     </svg>
                     Channeling Center
                 </button>
-                <button className="ecare-btn btn-login" onClick={() => navigate('/login')}>
-                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor">
-                        <path d="M11 7L9.6 8.4l2.6 2.6H2v2h10.2l-2.6 2.6L11 17l5-5-5-5zm9 12h-8v2h8c1.1 0 2-.9 2-2V5c0-1.1-.9-2-2-2h-8v2h8v14z" />
-                    </svg>
-                    Login
-                </button>
-                <button className="ecare-btn btn-signup" onClick={() => navigate('/signup')}>
-                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor">
-                        <path d="M15 12c2.21 0 4-1.79 4-4s-1.79-4-4-4-4 1.79-4 4 1.79 4 4 4zm-9-2V7H4v3H1v2h3v3h2v-3h3v-2H6zm9 4c-2.67 0-8 1.34-8 4v2h16v-2c0-2.66-5.33-4-8-4z" />
-                    </svg>
-                    Sign Up
-                </button>
+
+                {user ? (
+                    <>
+                        <div className="ecare-welcome">
+                            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor">
+                                <path d="M12 12c2.7 0 4.8-2.1 4.8-4.8S14.7 2.4 12 2.4 7.2 4.5 7.2 7.2 9.3 12 12 12zm0 2.4c-3.2 0-9.6 1.6-9.6 4.8v2.4h19.2v-2.4c0-3.2-6.4-4.8-9.6-4.8z" />
+                            </svg>
+                            <span>Welcome, <strong>{patientName}</strong></span>
+                        </div>
+                        <button className="ecare-btn btn-logout" onClick={handleLogout}>
+                            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor">
+                                <path d="M17 7l-1.41 1.41L18.17 11H8v2h10.17l-2.58 2.58L17 17l5-5-5-5zM4 5h8V3H4c-1.1 0-2 .9-2 2v14c0 1.1.9 2 2 2h8v-2H4V5z" />
+                            </svg>
+                            Logout
+                        </button>
+                    </>
+                ) : (
+                    <>
+                        <button className="ecare-btn btn-login" onClick={() => navigate('/login')}>
+                            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor">
+                                <path d="M11 7L9.6 8.4l2.6 2.6H2v2h10.2l-2.6 2.6L11 17l5-5-5-5zm9 12h-8v2h8c1.1 0 2-.9 2-2V5c0-1.1-.9-2-2-2h-8v2h8v14z" />
+                            </svg>
+                            Login
+                        </button>
+                        <button className="ecare-btn btn-signup" onClick={() => navigate('/signup')}>
+                            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor">
+                                <path d="M15 12c2.21 0 4-1.79 4-4s-1.79-4-4-4-4 1.79-4 4 1.79 4 4 4zm-9-2V7H4v3H1v2h3v3h2v-3h3v-2H6zm9 4c-2.67 0-8 1.34-8 4v2h16v-2c0-2.66-5.33-4-8-4z" />
+                            </svg>
+                            Sign Up
+                        </button>
+                    </>
+                )}
             </div>
         </nav>
     );
