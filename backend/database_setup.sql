ffabea2938b1f22c83c3585ad7c2104c4739c072
Nadeesha Malshan <malshannandanayaka@gmail.com>
2026-02-25 17:36:16 +0530

    updated

--- backend/database_setup.sql
+++ backend/database_setup.sql
@@ -70,16 +70,29 @@ CREATE TABLE IF NOT EXISTS doc_availability_slots (
 
 
 CREATE TABLE IF NOT EXISTS appointment_schedules (
-    id INT PRIMARY KEY AUTO_INCREMENT,
+    id INT AUTO_INCREMENT PRIMARY KEY,
     doctor_id INT NOT NULL,
-    schedule_date DATE,
-    start_time TIME,
-    end_time TIME,
-    max_patients INT,
+    schedule_date DATE NOT NULL,
+    start_time TIME NOT NULL,
+    end_time TIME NOT NULL,
+    max_patients INT NOT NULL,
+    price DECIMAL(10,2) NOT NULL DEFAULT 0.00,
     booked_count INT DEFAULT 0,
-    status VARCHAR(20) DEFAULT 'active',
+    status ENUM('active','full','cancelled') DEFAULT 'active',
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-    FOREIGN KEY (doctor_id) REFERENCES doctors(id)
+    FOREIGN KEY (doctor_id) REFERENCES doctors(id) ON DELETE CASCADE
+);
+
+CREATE TABLE IF NOT EXISTS appointments (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    schedule_id INT NOT NULL,
+    doctor_id INT NOT NULL,
+    patient_ID INT NOT NULL,
+    appointment_payment_status ENUM('pending','paid','failed','refunded') DEFAULT 'pending',
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    FOREIGN KEY (schedule_id) REFERENCES appointment_schedules(id) ON DELETE CASCADE,
+    FOREIGN KEY (doctor_id) REFERENCES doctors(id) ON DELETE CASCADE,
+    FOREIGN KEY (patient_ID) REFERENCES patients(id) ON DELETE CASCADE
 );
 
 
@@ -88,6 +101,7 @@ CREATE TABLE IF NOT EXISTS payments (
     internal_order_id VARCHAR(50) NOT NULL,
     patient_id INT NOT NULL,
     doctor_id INT NOT NULL,
+    appointment_schedule_id INT NOT NULL,
     appointment_id INT NOT NULL,
     payhere_payment_id VARCHAR(50) DEFAULT NULL,
     amount DECIMAL (10,2) NOT NULL,
@@ -99,4 +113,5 @@ CREATE TABLE IF NOT EXISTS payments (
     FOREIGN KEY (patient_id) REFERENCES patients(id),
     FOREIGN KEY (doctor_id) REFERENCES doctors(id),
     FOREIGN KEY (appointment_id) REFERENCES appointment_schedules(id)
+    FOREIGN KEY (appointment_schedule_id) REFERENCES appointment_schedules(id)
 )
