a459098d80a4766844a034176fe28997db0ec0a9
Sahan-Senadheera <sahansenadheera1@gmail.com>
2026-03-21 13:33:49 +0530

    update Doctor backend

--- backend/routes/authRoutes.js
+++ backend/routes/authRoutes.js
@@ -1,16 +1,10 @@
 const express = require('express');
 const router = express.Router();
 const authController = require('../controllers/authController');
-const doctorController = require('../controllers/doctorController');
 
 router.post('/signup', authController.registerPatient);
 router.post('/doctor/signup', authController.registerDoctor);
 router.post('/login', authController.login);
 router.post('/staff-login', authController.staffLogin);
-router.get('/doctors/specializations', doctorController.getSpecializations);
-router.get('/doctors', doctorController.getDoctors);
-router.delete('/doctor/:id', doctorController.deleteDoctorAccount);
-router.get('/doctor/:id', doctorController.getDoctorDetails);
-router.put('/doctor/:id', doctorController.updateDoctorProfile);
 
 module.exports = router;
