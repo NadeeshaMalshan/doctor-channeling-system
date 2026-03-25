2343d5b314c105bf6b79e2307bc17d49bdd1b362
IT24103522 <pamoddil7@gmail.com>
2026-03-25 08:55:51 +0530

    profile view color change

--- src/Components/Profile.js
+++ src/Components/Profile.js
@@ -165,7 +165,7 @@ const Profile = ({ patientId, onClose, onUpdate }) => {
     return (
         <div className="profile-overlay">
             <div className="profile-modal new-design">
-                <div className="profile-header">
+                <div className="profile-modal-header">
                     <div className="header-title">
                         <svg className="header-icon" viewBox="0 0 24 24" fill="#304f6d">
                             <path d="M12 2C6.48 2 2 6.48 2 12s4.48 10 10 10 10-4.48 10-10S17.52 2 12 2zm0 3c1.66 0 3 1.34 3 3s-1.34 3-3 3-3-1.34-3-3 1.34-3 3-3zm0 14.2c-2.5 0-4.71-1.28-6-3.22.03-1.99 4-3.08 6-3.08 1.99 0 5.97 1.09 6 3.08-1.29 1.94-3.5 3.22-6 3.22z"/>
